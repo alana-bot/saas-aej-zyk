e2ac251945aaffe2a85d6b2113e746f95edf7aa0
bot-maker <none>
2017-03-28 21:43:22 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
+addGreeting((user, response) => {
+    response.sendText('📢 🤖')
+})
+
 newScript()
   .dialog((sessions, response) => {
-    response.sendText('hello world');
+    If (sessions.message.type === 'text') {
+        switch (sessions.message.text) {
+            // solved better with intents!
+            case 'hello':
+            case 'hi':
+            case 'hey':
+                return response.sendText('hello world');
+            case 'echo':
+                response.sendText(sessions.message.text);
+                response.sendText(sessions.message.text);
+                response.sendText(sessions.message.text);
+                return;
+            default:
+                return response.sendText(sessions.message.text);
+        }
+    }
   });
   
